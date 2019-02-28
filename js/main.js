36c5aef63a48054b83fa556c0e7398df68090535
Prejith Premkumar <prajithpremg@gmail.com>
2019-03-01 00:48:07 +0530

    rework footer

--- js/main.js
+++ js/main.js
@@ -46,27 +46,6 @@
 		});
 	});
 
-	// Galery Owl
-	$('#galery-owl').owlCarousel({
-		items:1,
-		loop:true,
-		margin:0,
-		dots : false,
-		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		autoplay : true,
-		autoplaySpeed :500,
-		navSpeed :500,
-		responsive : {
-	    0 : {
-	       stagePadding : 0,
-	    },
-	    768 : {
-	        stagePadding : 120,
-	    }
-		}
-	});
-
 	// Parallax Background
 	$.stellar({
 		responsive: true
